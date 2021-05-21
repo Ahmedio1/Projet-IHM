8f6f52a2504ba32f809d34248eae3dfc98925fd0
Plouvin Patrick <Patrick62110@hotmail.com>
2021-05-21 12:31:03 +0200

    Patrick

---
+++ projet.py
@@ -0,0 +1,25 @@
+class plateau:
+    def __init__(self):
+        self.plateauJ=[[0]*9]*9
+    
+    def display(self):
+        return (self.plateauJ)
+    
+class caisse:
+    def __init__(self):
+        self.etat=False
+    
+    def setEtat(self):
+        self.etat=1
+    
+    def verif(self,lig,col):
+        
+        
+class trou:
+    def __init__(self):
+        self.etat=False
+
+
+plateau=plateau()
+print(plateau.display())
+
